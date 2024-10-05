b4e4256ddc9938e749c7ed861ae54bfba9e68ec7
d.kaliaskaruly <kaliaskaruly.d@adamants.kz>
2024-10-05 19:39:24 +0500

    page not component for todo client

--- todo-fullstack/todo-client/src/app/app.routes.ts
+++ todo-fullstack/todo-client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './layout/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //   {
+  //     path: '**',
+  //     component: NotFoundComponent,
+  //   },
+];
